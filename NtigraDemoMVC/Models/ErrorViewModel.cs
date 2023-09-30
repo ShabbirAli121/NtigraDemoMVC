074f8808f9912ef50715d2bf646cb9030779476c
ShabbirAli <shabbirali.m@peerbits.com>
2023-09-30 10:49:32 +0530

    Add project files.

---
+++ NtigraDemoMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NtigraDemoMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
